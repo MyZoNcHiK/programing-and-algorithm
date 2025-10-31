c6effc4b2218f08fcfa70e76873d99478979c1dc
MyZoNcHiK <maksimnazaruk737@gmail.com>
2025-10-31 08:25:20 +0000

    додав 9-difficult.cpp

---
+++ programing/lab9/9-difficult.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+
+int rnd(int a, int b) { return a + rand() % (b - a + 1); }
+
+int main() {
+    srand(time(0));
+    int prices[30];
+
+    for (int i = 0; i < 30; i++) {
+        prices[i] = rnd(100, 300);
+        cout << prices[i] << " ";
+    }
+    cout << endl;
+
+    int minPrice = prices[0];
+    int buyDay = 0, sellDay = 0;
+    int maxProfit = 0;
+
+    for (int i = 1; i < 30; i++) {
+        if (prices[i] - minPrice > maxProfit) {
+            maxProfit = prices[i] - minPrice;
+            sellDay = i;
+        }
+        if (prices[i] < minPrice) {
+            minPrice = prices[i];
+            buyDay = i;
+        }
+    }
+
+    cout << "Максимальний прибуток: " << maxProfit << endl;
+    cout << "Купити в день: " << buyDay + 1 << " за ціною " << prices[buyDay] << endl;
+    cout << "Продати в день: " << sellDay + 1 << " за ціною " << prices[sellDay] << endl;
+
+    return 0;
+}
