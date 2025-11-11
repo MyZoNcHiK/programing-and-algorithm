c9c0a922f906fd48fb27d27b35856c1eaf102ad1
MyZoNcHiK <maksimnazaruk737@gmail.com>
2025-11-11 16:54:03 +0000

    зробив робочий інтерпритатор(майже робочий)

--- randomExercise/brainFuck.cpp
+++ randomExercise/brainFuck.cpp
@@ -1,46 +1,54 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
 
 int head = 0;
-int a[32];
-void cycle(string cmd, int size);
-void baseSyntax(string cmd){
-    for(int i=0; i<(cmd.size()-1); i++){
-        switch(cmd[i]){
+int a[32] = {0};
+
+void baseSyntax(const string& cmd);
+
+void cycle(const string& cmd, int& i) {
+    int start = i;
+    string partCmd;
+    int depth = 1;
+
+    for (int j = i + 1; j < cmd.size(); j++) {
+        if (cmd[j] == '[') depth++;
+        else if (cmd[j] == ']') depth--;
+        if (depth == 0) {
+            partCmd = cmd.substr(i + 1, j - i - 1); 
+            i = j; 
+            break;
+        }
+    }
+
+    while (a[head] != 0) {
+        baseSyntax(partCmd);
+    }
+}
+
+void baseSyntax(const string& cmd) {
+    for (int i = 0; i < cmd.size(); i++) {
+        switch (cmd[i]) {
             case '+': a[head]++; break;
             case '-': a[head]--; break;
             case '>': head++; break;
-            case '<': head++; break;
-            case '[': cycle(cmd, cmd.size()-1); break; 
+            case '<': head--; break;
+            case '[': cycle(cmd, i); break;
+            default: break;
         }
     }
-    return;
 }
-void cycle(string cmd, int size){
-    int end;
-    string cmdInC;
-    for(int i=head; i<size; i++){
-        if(cmd[i] == '['){
-            head++;
-            cycle(cmd, cmd.size()-1);
-        } else if(cmd[i] == ']'){
-            end == i;    
-        }
-    }
-    for(int i = head+1; i<end; i++){
-       cmdInC[i-(head+1)] == cmd[i];
-    }
-    while(cmd[head]>0){
-        baseSyntax(cmdInC);
-    }
-    return;
-}
-int main(){
+
+int main() {
     string cmd;
     cin >> cmd;
+
     baseSyntax(cmd);
-    for(int x: a){cout << x << " ";}
+
+    for (int x : a) cout << x << " ";
     cout << endl;
     return 0;
 }
+
