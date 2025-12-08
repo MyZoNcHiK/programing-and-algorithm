aea245b8f1bc6efd95a18ce14449d7a2367fa29b
MyZoNcHiK <maksimnazaruk737@gmail.com>
2025-12-08 16:26:35 +0200

    додав lab15

--- programing/lab15/15.cpp
+++ programing/lab15/15.cpp
@@ -1,1 +1,51 @@
-#include<iostream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Company {
+    string name;
+    string industry;
+    string address;
+
+    void init(string n, string ind, string addr) {
+        name = n;
+        industry = ind;
+        address = addr;
+    }
+
+    void print(){
+        cout << "Назва: " << name << '\n'
+             << "Галузь: " << industry << '\n'
+             << "Адреса: " << address << '\n';
+    }
+};
+
+struct Employee {
+    string name;
+    string position;
+    Company company;
+
+    void init(string n, string pos, Company comp) {
+        name = n;
+        position = pos;
+        company = comp;
+    }
+
+    void print(){
+        cout << "Ім'я: " << name << '\n'
+             << "Посада: " << position << '\n';
+        company.print();
+    }
+};
+
+int main() {
+    Company comp;
+    comp.init("Corgun.Inc", "Інформаційні технології", "Балка");
+
+    Employee emp;
+    emp.init("Назарук Максим", "Software Engineer", comp);
+
+    emp.print();
+    return 0;
+}
+
