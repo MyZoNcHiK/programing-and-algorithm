3086404c336e673e505668aade553083df0275b9
MyZoNcHiK <maksimnazaruk737@gmail.com>
2025-12-08 16:32:03 +0200

    змінив lab15

--- programing/lab15/15.cpp
+++ programing/lab15/15.cpp
@@ -1,51 +1,27 @@
-#include <iostream>
-#include <string>
+#include<iostream>
+#include<cstring>
+#include<fstream>
+#include<regex>
 using namespace std;
 
-struct Company {
-    string name;
-    string industry;
-    string address;
-
-    void init(string n, string ind, string addr) {
-        name = n;
-        industry = ind;
-        address = addr;
-    }
-
-    void print(){
-        cout << "Назва: " << name << '\n'
-             << "Галузь: " << industry << '\n'
-             << "Адреса: " << address << '\n';
-    }
-};
-
-struct Employee {
-    string name;
-    string position;
-    Company company;
-
-    void init(string n, string pos, Company comp) {
-        name = n;
-        position = pos;
-        company = comp;
-    }
-
-    void print(){
-        cout << "Ім'я: " << name << '\n'
-             << "Посада: " << position << '\n';
-        company.print();
-    }
-};
-
 int main() {
-    Company comp;
-    comp.init("Corgun.Inc", "Інформаційні технології", "Балка");
-
-    Employee emp;
-    emp.init("Назарук Максим", "Software Engineer", comp);
-
-    emp.print();
+    ofstream ofile("sample1.txt");
+    ofile <<"111.111.111.111\ngsfehlkdgahklasglhkdxklhdvhk 123.5124.35.534 222.121.125.137\n wihgslrhglhs l11.111.111.111  gsr 555.555.555.555\n 222.121.125.133" << endl;
+    ofile.close();
+    ifstream file("sample1.txt");
+    string line;
+    regex ip_regex(R"(\b((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b)");
+    int uniq=0;
+    if(file.is_open()) {
+        while (std::getline(file, line)) {     
+           smatch match;
+           if(regex_search(line, match, ip_regex)) {
+                cout << "Found IP: " << match.str(0) << endl;
+                uniq++;
+            }
+        }       
+    }
+    file.close();
+    cout << "unique: " << uniq << endl;
     return 0;
 }
-
